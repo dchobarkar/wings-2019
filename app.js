0b72e32eb4246f76fd587df1218dee9d10369414
Darshan Chobarkar <dchobarkar@gmail.com>
2018-12-27 00:58:02 +0530

    events added

--- app.js
+++ app.js
@@ -10,7 +10,23 @@ app.get("/",function(req,res){
     res.render("landing");
 });
 app.get("/events",function(req,res){
-    res.render("events");
+    var events = [
+        {name:"SMART CITY DESIGN", img:"", FP:"5000", SP:"3000", TP:"2000"},
+        {name:"SURVEYING", img:"", FP:"", SP:"", TP:""},
+        {name:"EPITOME", img:"", FP:"5000", SP:"3000", TP:"2000"},
+        {name:"ELECTROBUZZ", img:"", FP:"3000", SP:"2000", TP:"1000"},
+        {name:"LATHE WAR", img:"", FP:"", SP:"", TP:""},
+        {name:"NITRORACING", img:"", FP:"", SP:"", TP:""},
+        {name:"CODING KBC", img:"", FP:"", SP:"", TP:""},
+        {name:"CODE DATE", img:"", FP:"", SP:"", TP:""},
+        {name:"VIRTUAL CAMPUS", img:"", FP:"", SP:"", TP:""},
+        {name:"ENERGY CONTRAPTION", img:"", FP:"", SP:"", TP:""},
+        {name:"PICK O’ RUN", img:"", FP:"", SP:"", TP:""},
+        {name:"CADSTER", img:"", FP:"", SP:"", TP:""},
+        {name:"WEB QUEST", img:"", FP:"", SP:"", TP:""},
+        
+    ]
+    res.render("events",{events:events});
 });
 app.get("/hackathon",function(req,res){
     res.render("hackathon");
